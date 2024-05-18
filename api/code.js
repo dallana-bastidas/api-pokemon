31ea9dc186b2df7ed3546ed456e062904732154a
Dallana Bastidas <cindydallana2822@gmail.com>
2024-05-18 16:58:29 -0500

    buscador funcional

--- api/code.js
+++ api/code.js
@@ -1,72 +1,88 @@
-let buscary = document.querySelector("#buscar")
-let input = document.querySelector("#myInput")
-let btnP = document.querySelector("#btnPrev")
-let btnN = document.querySelector("#btnNext")
+const URL_POKEMONES = "https://pokeapi.co/api/v2/pokemon";
 
-btnP.setAttribute(`data-url-pokemon`, ``)
-btnN.setAttribute(`data-url-pokemon`, `https://pokeapi.co/api/v2/pokemon?offset=20&limit=20`)
+const buscar = document.querySelector("#buscar");
+const input = document.querySelector("#myInput");
+const btnP = document.querySelector("#btnPrev");
+const btnN = document.querySelector("#btnNext");
 
-let form = document.querySelector("#pokeDex");
-form.addEventListener("submit", function (evento) {
-    evento.preventDefault();
-    let input = form.querySelector(`input`).value;
-    console.log(input)
-}
-)
+const form = document.querySelector("#pokeDex");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const input = form.querySelector(`input`).value;
+
+  const res = fetch(`${URL_POKEMONES}/${input.toLowerCase()}`);
+
+  res
+    .then((Promesa) => Promesa.json())
+    .then((infoPokemon) => {
+      let grilla = document.querySelector("#grilla-personajes");
+      grilla.innerHTML = `
+        <div class="col">
 
 
-btnN.addEventListener(`click`, function (e) {
-    let urlPoke = e.target.getAttribute(`data-url-pokemon`)
-    if (urlPoke != `null`) {
-        listar_pokemon(e.target.dataset.urlPokemon)
-    }
-}
-)
-
-btnP.addEventListener(`click`, function (e) {
-    if (e.target.dataset.urlPokemon != `null`) {
-        listar_pokemon(e.target.dataset.urlPokemon)
-    }
-}
-)
-
-listar_pokemon()
-
-function listar_pokemon(URL = 'https://pokeapi.co/api/v2/pokemon') {
-    let grilla = document.querySelector("#grilla-personajes")
-    grilla.innerHTML = ``
-    let llamdo = fetch(URL)
-    llamdo.then(Promesa => Promesa.json())
-        .then(informacion => {
-            informacion.results.forEach(pokemon => {
-                let urlpokemon = pokemon.url
-                let llamarPokomn = fetch(urlpokemon)
-                llamarPokomn.then(llammada => llammada.json())
-                    .then(infoPokemon => {
-                        grilla.innerHTML += `
-        <div class="col"> 
-    
-      
             <div class="card carta">
-    
+
             <div class="fondo">
                 <img src="${infoPokemon.sprites.other["official-artwork"].front_default}" class="pokemonImg img-container card-img-top mt-5 p-4" alt="...">
             </div>
-    
+
             <div class="card-body">
                 <p class="pokemonNom card-text">${infoPokemon.name}</p>
             </div>
             </div>
          </div>
-         `
-                    })
-            });
+         `;
+    });
+});
 
-            btnP.setAttribute(`data-url-pokemon`, informacion.previous)
-            btnN.setAttribute(`data-url-pokemon`, informacion.next)
+btnN.addEventListener(`click`, function (e) {
+  let urlPoke = e.target.getAttribute(`data-url-pokemon`);
+  if (urlPoke != `null`) {
+    listar_pokemon(e.target.dataset.urlPokemon);
+  }
+});
 
-        })
+btnP.addEventListener(`click`, function (e) {
+  if (e.target.dataset.urlPokemon != `null`) {
+    listar_pokemon(e.target.dataset.urlPokemon);
+  }
+});
+
+listar_pokemon();
+
+function listar_pokemon(urlPokemones = `${URL_POKEMONES}?offset=4&limit=4`) {
+  let grilla = document.querySelector("#grilla-personajes");
+  grilla.innerHTML = ``;
+  let llamado = fetch(urlPokemones);
+  llamado
+    .then((Promesa) => Promesa.json())
+    .then((informacion) => {
+      informacion.results.forEach((pokemon) => {
+        let urlPokemon = pokemon.url;
+        let llamarPokemon = fetch(urlPokemon);
+        llamarPokemon
+          .then((llamada) => llamada.json())
+          .then((infoPokemon) => {
+            grilla.innerHTML += `
+        <div class="col">
 
 
+            <div class="card carta">
 
+            <div class="fondo">
+                <img src="${infoPokemon.sprites.other["official-artwork"].front_default}" class="pokemonImg img-container card-img-top mt-5 p-4" alt="...">
+            </div>
+
+            <div class="card-body">
+                <p class="pokemonNom card-text">${infoPokemon.name}</p>
+            </div>
+            </div>
+         </div>
+         `;
+          });
+      });
+
+      btnP.setAttribute(`data-url-pokemon`, informacion.previous);
+      btnN.setAttribute(`data-url-pokemon`, informacion.next);
+    });
 }
